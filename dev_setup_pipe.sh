4e0ddc3b06f42ceb1b5cff8fb9c6b9c7c8b6f3db
damorosodaragona <darioamorosodaragona@gmail.com>
2020-10-04 23:02:16 +0200

    Update dev_setup_pipe.sh

--- dev_setup_pipe.sh
+++ dev_setup_pipe.sh
@@ -24,6 +24,7 @@ set -Ee
 cd $(dirname $0)
 TOP=$(pwd -L)
 if not contains -- $TERM xterm konsole xterm-256color konsole-256color st-256color dvtm-256color
+fi
 
 function clean_mycroft_files() {
     echo '
