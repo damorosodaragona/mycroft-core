48d531173cd128f69849f11a1d48f66d64f64572
damorosodaragona <darioamorosodaragona@gmail.com>
2020-10-04 23:20:43 +0200

    Update dev_setup_pipeline.sh

--- dev_setup_pipeline.sh
+++ dev_setup_pipeline.sh
@@ -24,8 +24,7 @@ set -Ee
 cd $(dirname $0)
 TOP=$(pwd -L)
 
-if not contains -- $TERM xterm konsole xterm-256color konsole-256color st-256color dvtm-256color
-fi
+case $TERM in xterm) TERM=xterm-256color;; esac
 
 function clean_mycroft_files() {
     echo '
