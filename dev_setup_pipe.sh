2a52606d899811faa21baa50271bb1948291a8e2
damorosodaragona <darioamorosodaragona@gmail.com>
2020-10-04 23:14:15 +0200

    Update dev_setup_pipe.sh

--- dev_setup_pipe.sh
+++ dev_setup_pipe.sh
@@ -23,7 +23,8 @@ set -Ee
 
 cd $(dirname $0)
 TOP=$(pwd -L)
-if not contains -- $TERM xterm konsole xterm-256color konsole-256color st-256color dvtm-256color
+if not contains -- "$TERM" ; then xterm konsole xterm-256color konsole-256color st-256color dvtm-256color
+
 
 
 function clean_mycroft_files() {
@@ -592,3 +593,4 @@ fi
 
 #Store a fingerprint of setup
 md5sum requirements/requirements.txt requirements/extra-audiobackend.txt requirements/extra-stt.txt requirements/extra-mark1.txt requirements/tests.txt dev_setup.sh > .installed
+fi
